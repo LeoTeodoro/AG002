2b03a98402f4596961d7419bc2d41bcd9a5816a5
LeoTeodoro <leoteodoro18@gmail.com>
2024-06-04 16:16:05 -0300

    adicionando o link para o vídeo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # AG002
 Repositório para o trabalho de AG002
+
+## LINK PARA O VÍDEO ##
+https://youtu.be/B4gsyruA5OU
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report
 
 #2)
